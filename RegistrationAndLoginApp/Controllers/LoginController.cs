39e4404526876a54e24c336c19811e5bffd35458
Shafiq Izwan <Shafiq Izwan@MSI>
2022-01-03 22:19:45 +0800

    Add project files.

---
+++ RegistrationAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationAndLoginApp.Models;
+
+namespace RegistrationAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "1111" && userModel.Password == "1111")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+            
+        }
+    }
+}
---
+++ RegistrationAndLoginApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegistrationAndLoginApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
